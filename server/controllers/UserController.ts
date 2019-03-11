2e44b752eac1d8bffe95881617b21035bfb02a8c
Vailorium <47310940+Vailorium@users.noreply.github.com>
2019-03-12 12:55:05 +1300

    code cleanup

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -10,30 +10,30 @@ declare const JWT_ISSUER: string;
 export class UserController {
 
 
-    @Get("/users")
-    @Header('Access-Control-Allow-Origin','*')
-    getAll(@Res() response) {
-    // response.send(401);
-    const token = sign({}, JWT_SECRET, {
-        audience: "Helol!",
-        expiresIn: "50 seconds", //expiry
-        issuer: JWT_ISSUER,
-        subject: "useremail@email.com"
-    });
-    const test = verify(token, JWT_SECRET);
-    return {token: test};
-    }
+    // @Get("/users")
+    // @Header('Access-Control-Allow-Origin','*')
+    // getAll(@Res() response) {
+    // // response.send(401);
+    // const token = sign({}, JWT_SECRET, {
+    //     audience: "Helol!",
+    //     expiresIn: "50 seconds", //expiry
+    //     issuer: JWT_ISSUER,
+    //     subject: "useremail@email.com"
+    // });
+    // const test = verify(token, JWT_SECRET);
+    // return {token: test};
+    // }
 
-    @Post("/login")
-    @Header('Access-Control-Allow-Origin','*')
-    @Header('Access-Control-Allow-Headers', 'Content-Type')
-    loginUser(@Res() res, @Req() req){
-        //verifyLogin()
-        const token = sign({userId:"0800-83-83-83"}, JWT_SECRET, {
-            expiresIn: "5 seconds"
-        });
-        return {token: token};
-    }
+    // @Post("/login")
+    // @Header('Access-Control-Allow-Origin','*')
+    // @Header('Access-Control-Allow-Headers', 'Content-Type')
+    // loginUser(@Res() res, @Req() req){
+    //     //verifyLogin()
+    //     const token = sign({userId:"0800-83-83-83"}, JWT_SECRET, {
+    //         expiresIn: "5 seconds"
+    //     });
+    //     return {token: token};
+    // }
 
     // @Post("/verify")
     // @Header("Access-Control-Allow-Origin","localhost:4200")
--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,8 @@ import * as Controllers from "./controllers"
 import * as cors from 'cors';
 import * as circular_json from 'circular-json'
 import { sign, verify, TokenExpiredError } from 'jsonwebtoken';
+import * as bodyParser from 'body-parser';
+
 
 declare const JWT_SECRET;
 const app = createExpressServer({
@@ -13,15 +15,21 @@ controllers: Object.values(Controllers) // we specify controllers we want to use
 app.use(cors({
     origin: 'http://localhost:4200'
 }));
-    
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 useExpressServer(app, {
     // controllers: Object.values(Controllers)
 });
     
-app.get('/test',cors(), (req, res, next) => {
-    res.json({ msg: 'coolio!'});
+app.get('/ping',cors(), (req, res, next) => {
+    res.json({ msg: 'pong!'});
 });
 
+app.get('/api/users', cors(), (req, res, next) => {
+    res.json([{firstName:"John", lastName: "Smith"},{firstName:"Jo", lastName:"Doe"}]);
+})
 app.post('/api/verify', cors(), (req, res, next) => {
     try{
         const verified = verify(req.headers.authorization, JWT_SECRET);
@@ -31,7 +39,22 @@ app.post('/api/verify', cors(), (req, res, next) => {
         return;
     }
     res.json({verified: true, msg: "Sent!"});
-})
+});
+
+app.post('/api/login', cors(), (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    // DATABASE CALL, GET USERID
+    const userId = "0800838383" //returned userId from database call
+    if(true){ //if user does exist
+        const token = sign({userId: userId}, JWT_SECRET, {expiresIn: "300 seconds"}); //change expiresIn
+        res.status(200);
+        res.json({ token: token });
+    }
+    else{
+        res.status(401);
+    }
+});
 // run express application on port 3000
 const server = app.listen(3000, () => {
     console.log("Server started on port " + server.address().port);
