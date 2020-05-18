ea10903fb697a663da938f2b5893b1f0dabbdb36
Isaac Lyne <IsaactimothyLyne@gmail.com>
2020-05-18 15:22:34 +1200

    Update NoticesController.ts

--- server/controllers/NoticesController.ts
+++ server/controllers/NoticesController.ts
@@ -29,9 +29,19 @@ export class NoticesController {
         const Language = req.body.Language;
         const NeedMic = req.body.NeedMic;
         const enddate = req.body.enddate;
-        request.query(`DELETE FROM lfgrequest WHERE RiotID='${RiotID}'`, (err, result) =>{
+        const PNeeded = Number(req.body.PlayersNeeded) -1 ;
+        const PlayersNeededs = String(PNeeded);
+        request.query(`DELETE FROM lfgrequests WHERE CONVERT (VARCHAR, RiotID)='${RiotID}'`, (err, result) =>{
             if (err) { console.log(err); }
-
+            if(PNeeded !== 0 ){
+                console.log("Minusing 1")
+                request.query(`insert into lfgrequests(RiotID, PlayersNeeded, Activity, Language, NeedMic, enddate) values('${RiotID}','${PlayersNeededs}','${Activity}','${Language}','${NeedMic}', '${enddate}')`, (err, result) => {
+                if (err) { console.log(err); }
+                });
+            }
+            else{
+                console.log("team Full");
+            }
             if (enddate !== undefined) {
                 setTimeout(() => {
                     new sql.Request().query(`DELETE FROM lfgrequests WHERE enddate='${enddate}'`, (err2, result2) => {
@@ -45,6 +55,28 @@ export class NoticesController {
         });
     }
 
+    Minus1 = async (req, res) => {
+        console.log("recieved Minus1 request");
+        const request = new sql.Request();
+        const RiotID = req.body.RiotID;
+        const PNeeded = Number(req.body.PlayersNeeded) -1 ;
+        const Activity = req.body.Activity;
+        const Language = req.body.Language;
+        const NeedMic = req.body.NeedMic;
+        const PlayersNeeded = String(PNeeded);
+        if(PNeeded != 0 ){
+// tslint:disable-next-line: max-line-length
+            request.query(`insert into lfgrequests(RiotID, PlayersNeeded, Activity, Language, NeedMic) values('${RiotID}','${PlayersNeeded}','${Activity}','${Language}','${NeedMic}')`, (err, result) => {
+            if (err) { console.log(err); }
+
+            res.json({msg: 'Notice Created'});
+            });
+        }
+        else{
+            console.log("team Full");
+        }
+    }
+
     addNotice = async (req, res) => {
         console.log("recieved Addnotice request");
         const request = new sql.Request();
@@ -53,19 +85,9 @@ export class NoticesController {
         const Activity = req.body.Activity;
         const Language = req.body.Language;
         const NeedMic = req.body.NeedMic;
-        const enddate = req.body.enddate;
         // tslint:disable-next-line: max-line-length
-        request.query(`insert into lfgrequests(RiotID, PlayersNeeded, Activity, Language, NeedMic, enddate) values('${RiotID}','${PlayersNeeded}','${Activity}','${Language}','${NeedMic}', '${enddate}')`, (err, result) => {
+        request.query(`insert into lfgrequests(RiotID, PlayersNeeded, Activity, Language, NeedMic) values('${RiotID}','${PlayersNeeded}','${Activity}','${Language}','${NeedMic}')`, (err, result) => {
             if (err) { console.log(err); }
-
-            if (enddate !== undefined) {
-                setTimeout(() => {
-                    new sql.Request().query(`DELETE FROM lfgrequests WHERE enddate='${enddate}'`, (err2, result2) => {
-                        if(err2) { console.log(err2); }
-
-                    });
-                }, (new Date(enddate).getTime() - new Date().getTime()));
-            }
 
             res.json({msg: 'Notice Created'});
         });
